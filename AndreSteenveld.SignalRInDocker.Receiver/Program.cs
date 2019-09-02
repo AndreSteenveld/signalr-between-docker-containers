f73623d45606ee0ce1b47d7b3c5e65087fe23489
André Steenveld <andre@fusion-electronics.nl>
2019-09-02 23:37:45 +0200

    Initialize the receiver project

---
+++ AndreSteenveld.SignalRInDocker.Receiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AndreSteenveld.SignalRInDocker.Receiver
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
